ba5a32ccbf12c6625d8ae678054636c689fab295
jaindivyam <divyamsethi198@gmail.com>
2025-07-10 22:42:40 +0530

    Add files via upload

---
+++ keypad.java
@@ -0,0 +1,31 @@
+import java.util.HashSet;
+public class keypad{
+
+
+    public static String[] keypad = {".", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tu", "vwx", "yz"};
+       
+    public static void printNumb(String str, int idx, String combination){
+
+           if(idx == str.length()) {
+        
+            System.out.println(combination);
+            return;
+        }
+
+        char currChar= str.charAt(idx);
+
+    String mapping= keypad[currChar-'0'];
+
+  for(int i=0; i<mapping.length(); i++){
+    printNumb(str, idx+1, combination+mapping.charAt(i));
+  }
+
+    }
+    public static void main(String args[]){
+        String str ="42";
+        printNumb(str, 0, " ");
+        
+    }
+    
+
+}
